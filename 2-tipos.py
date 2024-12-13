a17d77f2a83f2664e2e39e86009cd5e5b76a3b3a
DEVair Martins <devair.martins@gmail.com>
2024-12-12 23:05:02 -0300

    finalizando dia 1

--- 2-tipos.py
+++ 2-tipos.py
@@ -2,3 +2,13 @@ name = "Resident Evil 4 Remake"
 yearLaunch = 2023
 gamePrice = 300.00
 planeInclued = False
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planeInclued)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(gamePrice))
+print(type(planeInclued))
